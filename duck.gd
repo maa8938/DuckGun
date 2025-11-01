7d693bb903b2ab06a56b7bf2ca8ccc628076229a
munim1216 <145690615+munim1216@users.noreply.github.com>
2025-11-01 15:57:58 -0400

    PELLETS SHOOT
    WOOO

--- duck.gd
+++ duck.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 
 const SPEED = 250
-var current_theta = -1
-var quadrant
+var pellet_param = []
 @onready var PELLET = preload("res://pellet.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,6 +22,8 @@ func _process(delta: float) -> void:
 	
 	var current_theta = atan(y/x)
 	
+	print(current_theta)
+	
 	var delta_x = cos(current_theta) * SPEED * delta
 	var delta_y = sin(current_theta) * SPEED * delta
 	
@@ -32,15 +33,8 @@ func _process(delta: float) -> void:
 	if not above:
 		delta_y *= -1
 	
-	if not left and above:
-		quadrant = Globals.Q1
-	elif left and above:
-		quadrant = Globals.Q2
-	elif left and not above:
-		quadrant = Globals.Q3
-	elif not left and not above:
-		quadrant = Globals.Q4
-		
+	pellet_param = [current_theta, delta_x, delta_y]
+	
 	position.x += delta_x
 	position.y += delta_y
 	
@@ -48,15 +42,14 @@ func _process(delta: float) -> void:
 
 func blast():
 	var pellet = PELLET.instantiate()
-	pellet.Pellet(position, current_theta, quadrant)
+	pellet.Pellet(position, pellet_param)
 	get_tree().current_scene.add_child(pellet)
 	print(get_parent().get_tree_string_pretty())
 
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("left_click"):
-		#blast()
-		print("blast")
+		blast()
 	
 	
 	
