0476f1fdb638af866f8f3c9bcb65c225aa7182ab
Matthew Guo <matthewguo250@gmail.com>
2025-11-01 23:19:04 -0400

    further advancements in generation

---
+++ hedge_maze_map.gd
@@ -0,0 +1,5 @@
+extends TileMapLayer
+
+func getTileSet():
+	return tile_set
+	print("brther")
--- maze_generator.gd
+++ maze_generator.gd
@@ -1,8 +1,19 @@
 extends Node
 
+const horzlineIndex = 0
+const cornerIndex = 2
+const normalRoomIndex = 3
+const midWallRoomIndex = 4;
 
 
 func _ready() -> void:
 	# called whenever the object enters the scene
-	pass
+	
 	# randomly generates 
+	var map = $HedgeMazeMap.get_script()
+	
+	var pattern = map.getTileSet()
+	
+	# place 10 random rooms around the area
+		# make sure that there's no intersection
+	# var normRoom = map.get_pattern(normalRoomIndex)
