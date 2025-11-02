a1f294a828857c8e08eb28bb614f2e64dbeeeb9a
Matthew Guo <matthewguo250@gmail.com>
2025-11-02 01:39:21 -0400

    map_gen update
    make sure that this thing is scalable

--- maze_generator.gd
+++ maze_generator.gd
@@ -3,10 +3,11 @@ extends Node
 const horzlineIndex = 0
 const cornerIndex = 2
 const normalRoomIndex = 3
-const RoomSize = 100
+const RoomSize = 60
 const midWallRoomIndex = 4
 const gridSize = 800
 const grassTileID = 0
+const numOfRooms = 30
 
 @onready var tilemap = $HedgeMazeMap
 
@@ -29,21 +30,22 @@ func _ready() -> void:
 	var position
 	var positionCheck
 	
-	for i in range(15):
+	for i in range(numOfRooms):
 		positionCheck = false
 		while positionCheck == false:
 			var isInRange = false
-			randX = randi_range(-gridSize / 2, (gridSize / 2) - RoomSize)
-			randY = randi_range(-gridSize / 2, (gridSize / 2) - RoomSize)
+			randX = randi_range(-gridSize / 2 + RoomSize / 2, (gridSize / 2) - RoomSize / 2)
+			randY = randi_range(-gridSize / 2 + RoomSize / 2, (gridSize / 2) - RoomSize / 2)
 			position = Vector2i(randX, randY)
 			for j in range(roomArray.size()):
-				if position.x in roomArray.get(j).get(0) or position.y in roomArray.get(j).get(1):
+				var currentRoom = roomArray.get(j)
+				if (position.x > currentRoom["x1"] and position.x < currentRoom["x2"]) or (position.y > currentRoom["y1"] and position.y < currentRoom["y2"]): 
 					isInRange = true
 					break
 			if isInRange == false:
 				positionCheck = true
 				tilemap.set_pattern(position, normalRoom)
-				roomArray.append([range(position.x, position.x + RoomSize), range(position.y, position.y + RoomSize)])
+				roomArray.append({"x1": position.x - RoomSize / 2, "x2": position.x + RoomSize / 2, "y1": position.y - RoomSize / 2, "y2": position.y + RoomSize / 2})
 			# TODO: Change so it randomly picks between a random index of unique rooms
 		
 	# puts down a pattern
